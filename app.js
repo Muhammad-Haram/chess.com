67798a391e274f5505f24085bb96e700ba00e87c
Muhammad Haram <haramh643@gmail.com>
2024-08-26 01:01:04 +0500

    backend

--- app.js
+++ app.js
@@ -21,6 +21,41 @@ app.get("/", (req, res) => {
 
 io.on("connection", function (uniqueSocket) {
     console.log("connected");
+
+
+    if (!players.white) {
+        players.white = uniqueSocket.id;
+        uniqueSocket.emit("playerRole", "w")
+    } else if (!players.black) {
+        players.black = uniqueSocket.id;
+        uniqueSocket.emit("playerRole", "b");
+    } else {
+        uniqueSocket.emit("spectatorRole");
+    };
+
+    uniqueSocket.on("disconnect", function () {
+
+        if (uniqueSocket.id === players.white) {
+            delete players.white;
+        } else if (uniqueSocket.id === players.black) {
+            delete players.black;
+        }
+
+    });
+
+    uniqueSocket.on("move", (move) => {
+        try {
+
+            if (chess.turn() === "w" && players.white === uniqueSocket.id) return;
+            if (chess.turn() === "b" && players.black === uniqueSocket.id) return;
+
+
+        } catch (error) {
+            console.log(error.message);
+        }
+    })
+
+
 })
 
 server.listen(3000, function () {
